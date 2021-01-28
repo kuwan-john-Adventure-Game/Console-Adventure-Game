bb5c2848529c12bf88b8646a43f528a34dcfd4f8
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-01-28 00:07:29 -0600

    added color and potion edits

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -1,38 +1,42 @@
 import java.util.Scanner;
 
-public class adventuregame {
+public class AdventureGame {
     public static void main(String[] args) throws InterruptedException {
+
         int heroHP = 100;
-        int enemyHP = 75;
-//        int ATK = (int) (Math.random() * 35);
+        int enemyHP = 5;
         int potion = 15;
-//        int enemyATK = (int) (Math.random() * 20);
+        boolean escaped = false;
 
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your name, adventurer?");
         String name = scan.next();
         name = name.substring(0, 1).toUpperCase() + name.substring(1);
-//        System.out.println("This is a battling game, " + name + ". Are you willing to step into the Java Battler?");
-        String answer = scan.nextLine().toLowerCase();
-        System.out.println(answer);
-//        if (answer.startsWith("y")) {
-                System.out.print("Welcome to the Java Battler, ");
-                Thread.sleep(1000);
-                System.out.println(name + ".");
-                Thread.sleep(1500);
+        Thread.sleep(800);
+        System.out.println("Hello, " + name + ".");
+        Thread.sleep(1000);
+        System.out.println("Are you willing to step into the Java Battler?");
+        String answer = scan.next().toLowerCase();
+
+        if (answer.startsWith("y")) {
+                Thread.sleep(500);
+                System.out.println("Welcome to the Java Battler.");
+                Thread.sleep(1200);
                 System.out.println("Oh, no..");
                 Thread.sleep(800);
                 System.out.print("An enemy approaches! ");
                 Thread.sleep(1500);
                 System.out.println("..quick, do something!");
                 Thread.sleep(1200);
-                System.out.println(name + "'s HP: " + heroHP + " <---||---> Enemy HP: " + enemyHP);
+                System.out.printf("{%s's HP: \033[0;32m%s\u001B[0m <===||===> \033[0;32m%d\u001B[0m :Enemy's HP}%n", name, heroHP, enemyHP);
+//                System.out.println(name + "'s HP: " + heroHP + " <---||---> Enemy HP: " + enemyHP);
             do {
                 int ATK = (int) (Math.random() * 35);
                 int enemyATK = (int) (Math.random() * 20);
                 Thread.sleep(500);
-                System.out.println("[A]ttack\n[P]otion\n[E]scape");
-                String response = scan.nextLine().trim();
+                System.out.format("\033[0;33m[A]ttack\n\033[0;34m[P]otion\n\033[0;35m[E]scape\u001B[0m%n");
+//                System.out.println(name + "'s HP: " + heroHP + " <---||---> Enemy HP: " + enemyHP);
+                String response = scan.next().trim();
 
                 if (response.toLowerCase().startsWith("a")) {
                     enemyHP -= ATK;
@@ -43,17 +47,25 @@ public class adventuregame {
                     System.out.println("The enemy attacks for: " + enemyATK + "!");
                     Thread.sleep(1200);
                     heroHP = heroHP - enemyATK;
-                    System.out.println(name + "'s HP: " + heroHP + " <--||--> Enemy HP: " + enemyHP);
+//                    System.out.println(name + "'s HP: " + heroHP + " <---||---> Enemy HP: " + enemyHP);
+                    System.out.printf("{%s's HP: \033[0;32m%s\u001B[0m <===||===> \033[0;32m%d\u001B[0m :Enemy's HP}%n", name, heroHP, enemyHP);
                 } else if (response.toLowerCase().startsWith("p")) {
                     Thread.sleep(500);
                     System.out.println("You drink a potion and gain " + potion + " health.");
-                    heroHP += potion;
-
                     Thread.sleep(1000);
-                    heroHP = heroHP - enemyATK;
+
+                    if (heroHP + potion >= 100) {
+                        heroHP = 100;
+                    } else {
+                        heroHP += potion;
+                    }
+
+                    heroHP -= enemyATK;
                     System.out.println("The enemy attacks for: " + enemyATK + "!");
                     Thread.sleep(1200);
-                    System.out.println(name + "'s HP: " + heroHP + " <--||--> Enemy HP: " + enemyHP);
+//                    System.out.println(name + "'s HP: " + heroHP + " <--||--> Enemy HP: " + enemyHP);
+                    System.out.printf("{%s's HP: \033[0;32m%s\u001B[0m <===||===> \033[0;32m%d\u001B[0m :Enemy's HP}%n", name, heroHP, enemyHP);
+
                 } else if (response.toLowerCase().startsWith("e")) {
                     Thread.sleep(500);
                     System.out.println("You're running?");
@@ -62,20 +74,27 @@ public class adventuregame {
                     Thread.sleep(1200);
                     System.out.println("You're no Hero of mine!");
                     Thread.sleep(1500);
+                    escaped = true;
                     break;
                 } else {
                     System.out.println("Please enter a valid response.");
                 }
             } while (enemyHP >= 0 && heroHP >= 0);
+
+            if (!escaped) {
+                System.out.println();
                 Thread.sleep(1500);
+                System.out.print(".");
+                Thread.sleep(300);
+                System.out.print(".");
+                Thread.sleep(300);
+                System.out.print(".");
+                Thread.sleep(500);
                 System.out.println("Enemy defeated.");
                 Thread.sleep(1500);
-                System.out.print("You are the true Java Hero! ");
-                Thread.sleep(1500);
-                System.out.println("All hail " + name + "!!");
-                Thread.sleep(1500);
+                System.out.printf("Nice work, \033[0;32m%s\u001B[0m!%n", name);
+                Thread.sleep(2000);
+            }
         }
     }
-//}
-// need to figure out how to make it so that after a certain amount of tries the enemy attacks
-//fix the health for enemy
+}
